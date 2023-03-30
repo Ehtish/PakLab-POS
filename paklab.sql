e1d4cbdfb794478e6060d24fd6f4301587575b4b
Ehtisham Ali <40139972+Ehtish@users.noreply.github.com>
2023-03-30 14:57:47 +0500

    dummy data for understanding

---
+++ paklab.sql
@@ -0,0 +1,166 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 30, 2023 at 11:56 AM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `paklab`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `income`
+--
+
+CREATE TABLE `income` (
+  `id` int(11) NOT NULL,
+  `date` datetime NOT NULL,
+  `total_sales` int(11) UNSIGNED NOT NULL DEFAULT 0,
+  `total_cost` int(11) UNSIGNED NOT NULL DEFAULT 0,
+  `profit` int(11) GENERATED ALWAYS AS (`total_sales` - `total_cost`) STORED
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `income`
+--
+
+INSERT INTO `income` (`id`, `date`, `total_sales`, `total_cost`) VALUES
+(3, '2023-03-28 01:28:53', 320, 200),
+(4, '2023-03-28 01:42:40', 300, 200);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `brand_name` varchar(255) NOT NULL,
+  `description` text DEFAULT NULL,
+  `sales_price` int(11) NOT NULL,
+  `cost` int(11) NOT NULL,
+  `stock` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `brand_name`, `description`, `sales_price`, `cost`, `stock`) VALUES
+(10, 'Charger', 'samsung', 'this is description', 100, 50, 12),
+(11, 'Handsfree', 'oppo', '', 120, 100, 10),
+(12, 'gorilla glass', 'samsung', '', 300, 250, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales`
+--
+
+CREATE TABLE `sales` (
+  `id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `sale_date` datetime NOT NULL DEFAULT current_timestamp(),
+  `price` int(11) DEFAULT NULL,
+  `quantity` int(11) NOT NULL,
+  `total` int(11) GENERATED ALWAYS AS (`price` * `quantity`) STORED
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `sales`
+--
+
+INSERT INTO `sales` (`id`, `product_id`, `sale_date`, `price`, `quantity`) VALUES
+(16, 10, '2023-03-15 05:16:43', 100, 2),
+(20, 10, '2023-03-30 02:41:52', 100, 3),
+(21, 10, '2023-03-29 02:42:47', 100, 5);
+
+--
+-- Triggers `sales`
+--
+DELIMITER $$
+CREATE TRIGGER `delete_sales_trigger` AFTER DELETE ON `sales` FOR EACH ROW BEGIN
+    UPDATE products SET stock = stock + OLD.quantity WHERE id = OLD.product_id;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `sales_trigger` AFTER INSERT ON `sales` FOR EACH ROW BEGIN
+    UPDATE products SET stock = stock - NEW.quantity WHERE id = NEW.product_id;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `update_sales_trigger` AFTER UPDATE ON `sales` FOR EACH ROW BEGIN
+    UPDATE products SET stock = stock + OLD.quantity - NEW.quantity WHERE id = NEW.product_id;
+END
+$$
+DELIMITER ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `income`
+--
+ALTER TABLE `income`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sales`
+--
+ALTER TABLE `sales`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_sales_product` (`product_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `income`
+--
+ALTER TABLE `income`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `sales`
+--
+ALTER TABLE `sales`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
