f858bdd74d448e8d95eb993e52c04c272fa04c35
Ehtisham Ali <40139972+Ehtish@users.noreply.github.com>
2023-03-30 14:28:44 +0500

    include links in header file

--- inc/header.php
+++ inc/header.php
@@ -7,6 +7,17 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- data table css -->
+    <link
+      rel="stylesheet"
+      type="text/css"
+      href="https://cdn.datatables.net/1.11.4/css/jquery.dataTables.min.css"
+    />
+    <link
+      rel="stylesheet"
+      type="text/css"
+      href="https://cdn.datatables.net/buttons/2.2.2/css/buttons.dataTables.min.css"
+    />
     <title><?php echo ucwords($active_page); ?></title>
   </head>
   <body>
